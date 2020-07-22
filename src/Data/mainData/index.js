73035582b102f46c1b6cc469605d22c113001e04
qiaochunliu77 <1358238105@qq.com>
2020-07-22 16:39:55 +0800

    7-22主页增加内容

--- src/Data/mainData/index.js
+++ src/Data/mainData/index.js
@@ -1,1 +1,112 @@
-export const classify = ['擦玻璃', '空调清洗', '修电脑', '修手机', '空调移机', '洗窗帘', '修灯具']
+export const classify = ['擦玻璃', '空调清洗', '修电脑', '修手机', '空调移机', '洗窗帘', '修灯具'];
+export const rotationImg = [{
+    id: 1,
+    picUrl: 'https://yanxuan.nosdn.127.net/e1d32c538a9fcf420411592746098ad2.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+},
+{
+    id: 2,
+    picUrl: 'https://yanxuan.nosdn.127.net/7e01b30c2c440e118cf09e14c7a69266.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+},
+{
+    id: 3,
+    picUrl: 'https://yanxuan.nosdn.127.net/94408b4d014ce6774e8f768bdf9b6f60.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+}, {
+    id: 4,
+    picUrl: 'https://yanxuan.nosdn.127.net/e14d684c9dc43de2af5215c3d49b6870.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+},
+
+{
+    id: 5,
+    picUrl: 'https://yanxuan.nosdn.127.net/7e01b30c2c440e118cf09e14c7a69266.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+},
+{
+    id: 6,
+    picUrl: 'https://yanxuan.nosdn.127.net/94408b4d014ce6774e8f768bdf9b6f60.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+}, {
+    id: 7,
+    picUrl: 'https://yanxuan.nosdn.127.net/e14d684c9dc43de2af5215c3d49b6870.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
+    linkUrl: 'http://localhost:3000/main'
+}
+]
+export const menuBarData = [
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/4847e41c9c4c23b11c252815df12d42a.png?x-oss-process=image/auto-orient,0/format,webp',
+        type:'日常保洁'
+    },{
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/64b0a24bebec5e986b7523d24f38c9e8.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/9b04c448bf5a2e53407227abb39b27dc.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/c16a42b496dad271fc602eae789ce1bf.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/01a443c96c08e02cd32ddd9958f82292.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/4847e41c9c4c23b11c252815df12d42a.png?x-oss-process=image/auto-orient,0/format,webp',
+        type:'日常保洁'
+    },{
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/64b0a24bebec5e986b7523d24f38c9e8.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/9b04c448bf5a2e53407227abb39b27dc.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/c16a42b496dad271fc602eae789ce1bf.png',
+        type:'日常保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/01a443c96c08e02cd32ddd9958f82292.png',
+        type:'日常保洁'
+    }
+]
+export const menuBarData2 = [
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/4847e41c9c4c23b11c252815df12d42a.png?x-oss-process=image/auto-orient,0/format,webp',
+        type:'购卡有礼'
+    },{
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/64b0a24bebec5e986b7523d24f38c9e8.png',
+        type:'空调清洗'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/9b04c448bf5a2e53407227abb39b27dc.png',
+        type:'除菌保洁'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/c16a42b496dad271fc602eae789ce1bf.png',
+        type:'领优惠券'
+    },
+    {
+        id:1,
+        picUrl:'https://images.daojia.com/dop/custom/c16a42b496dad271fc602eae789ce1bf.png',
+        type:'全部服务 '
+    }
+]
--- src/components/main/rotationChart/RotationChart.jsx
+++ src/components/main/rotationChart/RotationChart.jsx
@@ -1,64 +1,39 @@
 import React, { Component } from 'react';
 import './RotationChart.css';
 import Swiper from 'swiper';
-import 'swiper/swiper-bundle.min.css';
+import "swiper/css/swiper.min.css";
+import { rotationImg } from '../../../Data/mainData/index'
+
 class RotationChart extends Component {
-    state = {
-        img: [{
-            id: 1,
-            picUrl: 'https://yanxuan.nosdn.127.net/e1d32c538a9fcf420411592746098ad2.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
-            linkUrl: 'http://localhost:3000/main'
-        },
-        {
-            id: 2,
-            picUrl: 'https://yanxuan.nosdn.127.net/7e01b30c2c440e118cf09e14c7a69266.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
-            linkUrl: 'http://localhost:3000/main'
-        },
-        {
-            id: 3,
-            picUrl: 'https://yanxuan.nosdn.127.net/94408b4d014ce6774e8f768bdf9b6f60.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
-            linkUrl: 'http://localhost:3000/main'
-        }, {
-            id: 4,
-            picUrl: 'https://yanxuan.nosdn.127.net/e14d684c9dc43de2af5215c3d49b6870.jpg?type=webp&amp;imageView&amp;quality=75&amp;thumbnail=750x0',
-            linkUrl: 'http://localhost:3000/main'
-        }
-        ]
-    }
     componentDidMount() {
-        this.initSwiper();
-    }
-    initSwiper() {
         this.mySwiper = new Swiper('.swiper-container', {
             loop: true,
             autoplay: {
                 delay: 1000,
             },
             pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-                    clickable: true,
+                el: '.swiper-pagination'
             }
         })
     }
     render() {
         return (
-            <div className="box">
-                <div class="swiper-container">
-                    <div class="swiper-wrapper ">
+            <div className="rotation-box">
+                <div className="swiper-container">
+                    <div className="swiper-wrapper ">
                         {
-                            this.state.img.map((item) => {
+                            rotationImg.map((item, index) => {
                                 return (
-                                    <div className="swiper-slide" key={item.id}>
+                                    <div className="swiper-slide" key={index}>
                                         <a href={item.linkUrl} >
-                                            <img src={item.picUrl} alt="" width="100%" height="100%" />
-                                        </a> 
+                                            <img className='rotationChart-img' src={item.picUrl} alt="" width="100%" height="100%" />
+                                        </a>
                                     </div>
                                 )
                             })
                         }
                     </div>
-                    <div class="swiper-pagination"></div>
+                    <div className="swiper-pagination"></div>
                 </div>
             </div>
         );
